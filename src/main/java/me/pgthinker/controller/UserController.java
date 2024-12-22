edd579ef193986d6b99b32f87118e7ecd526b518
bodyzxy <129066769+bodyzxy@users.noreply.github.com>
2024-12-22 12:35:40 +0800

    update

--- src/main/java/me/pgthinker/controller/UserController.java
+++ src/main/java/me/pgthinker/controller/UserController.java
@@ -1,6 +1,8 @@
 package me.pgthinker.controller;
 
 import lombok.RequiredArgsConstructor;
+import me.pgthinker.common.BaseResponse;
+import me.pgthinker.common.ResultUtils;
 import me.pgthinker.model.entity.UserDO;
 import me.pgthinker.model.vo.BaseDelete;
 import me.pgthinker.model.vo.UserReq;
@@ -18,18 +20,18 @@ public class UserController {
     private final UserService userService;
 
     @PostMapping("/delete")
-    public List<Long> delete(@RequestBody BaseDelete baseDelete){
-        return userService.delete(baseDelete);
+    public BaseResponse<List<Long>> delete(@RequestBody BaseDelete baseDelete){
+        return ResultUtils.success(userService.delete(baseDelete));
     }
 
     @PostMapping("/update")
-    public Long update(@RequestBody UserDO userDO){
-        return userService.update(userDO);
+    public BaseResponse<Long> update(@RequestBody UserDO userDO){
+        return ResultUtils.success(userService.update(userDO));
     }
 
     @PostMapping("/getUser")
-    public Pageable listOfPage(@RequestBody UserReq userReq){
-        return userService.listOfPage((Pageable) userReq);
+    public BaseResponse<Pageable> listOfPage(@RequestBody UserReq userReq){
+        return ResultUtils.success(userService.listOfPage((Pageable) userReq));
     }
 
 
